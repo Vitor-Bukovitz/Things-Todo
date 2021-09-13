da5995b00e6b6403d78ab6fd55c24d1c28043676
Vitor Bukovitz <vitorbukovitz@hotmail.com>
2021-09-13 19:55:43 -0300

    feat: initial project setup

---
+++ ThingsTodo/ThingsTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsTodoApp.swift
+//  ThingsTodo
+//
+//  Created by PremierSoft on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ThingsTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
