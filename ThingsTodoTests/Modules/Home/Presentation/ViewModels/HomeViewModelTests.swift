9f78df0920a8ab49ce61238d14b638c51536fb25
Vitor Bukovitz <vitorbukovitz@hotmail.com>
2021-09-13 21:17:45 -0300

    remove: removed main test feat: added HomeViewModelTests

---
+++ ThingsTodoTests/Modules/Home/Presentation/ViewModels/HomeViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModelTests.swift
+//  ThingsTodoTests
+//
+//  Created by PremierSoft on 13/09/21.
+//
+
+import XCTest
+@testable import ThingsTodo
+
+class HomeViewModelTests: XCTestCase {
+
+}
