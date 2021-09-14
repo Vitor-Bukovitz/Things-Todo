9d5ea561b4ac0833a50387e2755d1d930bde91fe
Vitor Bukovitz <vitorbukovitz@hotmail.com>
2021-09-13 21:16:07 -0300

    feat: added color extension, with primaryColor

---
+++ ThingsTodo/Common/Extensions/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  ThingsTodo
+//
+//  Created by PremierSoft on 13/09/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryColor = Color(.sRGB, red: 0.03, green: 0.11, blue: 0.18, opacity: 1)
+}
