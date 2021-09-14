e7b9badf716a976e4476edf5f9288951d841c823
Vitor Bukovitz <vitorbukovitz@hotmail.com>
2021-09-13 21:19:49 -0300

    feat: todo model

---
+++ ThingsTodo/Modules/Home/Domain/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  ThingsTodo
+//
+//  Created by PremierSoft on 13/09/21.
+//
+
+import Foundation
+
+struct Todo {
+    var id = UUID()
+    let text: String
+    let isCompleted: Bool
+}
